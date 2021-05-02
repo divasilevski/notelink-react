e3229e2e69d3f650195194dc1d1ee81c177ed70e
Dmitry Vasilevsky <divasilevski@gmail.com>
2021-05-02 22:23:28 +0300

    Alert context

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,21 @@
-export const Alert = ({ alert }) => {
+import { useContext } from "react"
+import { AlertContext } from "../context/alert/alertContext"
 
-  if (!alert) return null
+export const Alert = () => {
+  const { alert, hide } = useContext(AlertContext)
+
+  if (!alert.visible) return null
 
   return (
     <div className={`alert alert-${alert.type || 'warning'} alert-desmissible`}>
-      <strong>Внимание!</strong>
+      <strong>Внимание!&nbsp;</strong>
       {alert.text}
-      <button type="button" className="close" data-dismiss="alert" aria-label="close">
+      <button
+        type="button"
+        className="close"
+        aria-label="close"
+        onClick={hide}
+      >
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
